dc2c97c5e730622c31821cfe5a0d9edbf5e920b8
Cameron Dimitroff <cdimitro@e1z3r7p25.42.us.org>
2019-04-24 11:11:16 -0700

    yo

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cdimitro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/13 12:45:47 by cdimitro          #+#    #+#             */
-/*   Updated: 2019/04/17 23:34:01 by cdimitro         ###   ########.fr       */
+/*   Updated: 2019/04/24 11:10:12 by cdimitro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 #include <sys/stat.h>
 #include <stdio.h>
 
+
+
 int	main(int ac, char **av)
 {
 	int		fd;
@@ -25,9 +27,11 @@ int	main(int ac, char **av)
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
-		get_next_line(fd, &line);
-		printf("%s\n", line);
-		free(line);
+		while (get_next_line(fd, &line) > 0)
+		{
+			printf("%s\n", line);
+			free(line);
+		}
 		close(fd);
 	}
 	return (0);
